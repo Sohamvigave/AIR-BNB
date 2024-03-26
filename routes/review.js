8c341a1155ce4465a7eee537c7bb8e9983f65a1b
soham vigave <sohamvigave@gmail.com>
2024-03-26 16:06:42 +0530

    p2e authorization for /review part 1

--- routes/review.js
+++ routes/review.js
@@ -4,14 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 // POST REVIEW ROUTE
 router.post("/",
+    isLoggedIn,
     validateReview,
     wrapAsync(async (req, res) => {
         let listing = await Listing.findById(req.params.id);
         let newReview = new Review(req.body.review);
+        newReview.author = req.user._id;
+        console.log(newReview);
         listing.reviews.push(newReview);
         await newReview.save();
         await listing.save();
