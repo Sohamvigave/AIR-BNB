22d87e43da033ebec2b7b7fa2fe53a9c54dad8f8
soham vigave <sohamvigave@gmail.com>
2024-04-04 15:48:13 +0530

    p3b map marker

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,11 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [73.9580, 17.2378], // starting position [lng, lat]
+    center: coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(coordinates) // listing.geometry.coordinates
+    .addTo(map);
