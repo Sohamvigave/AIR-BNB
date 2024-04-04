64097ce047df60b874eb7334d7c187e1a867ec4c
soham vigave <sohamvigave@gmail.com>
2024-04-04 16:15:55 +0530

    p3b map Popup

--- public/js/map.js
+++ public/js/map.js
@@ -2,11 +2,17 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: listing.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker({ color: "red" })
-    .setLngLat(coordinates) // listing.geometry.coordinates
+    .setLngLat(listing.geometry.coordinates) // listing.geometry.coordinates
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h4>${listing.title}</h4><p>Exact location will be provide after booking</p>`
+        )
+    )
     .addTo(map);
